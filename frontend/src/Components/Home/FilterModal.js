cb94d9749420a945e5f9a888a841256aa11b93ed
developer-metalhead <satwikshah2000@gmail.com>
2024-03-09 19:33:51 +0530

    27 Feb Half

--- frontend/src/Components/Home/FilterModal.js
+++ frontend/src/Components/Home/FilterModal.js
@@ -47,7 +47,71 @@ const FilterModal = ({ selectedFilters, onFilterChange, onClose }) => {
     onFilterChange("amenities", amenities);
     onClose();
   };
-  return <div>FilterModal</div>;
+
+  const propertyTypeOptions = [
+    { value: "House", label: "House", icon: "home" },
+    { value: "Flat", label: "Flat", icon: "apartment" },
+    { value: "Hotel", label: "Hotel", icon: "meeting_room" },
+    { value: "Guest House", label: "Guest House", icon: "hotel" },
+  ];
+
+  const roomTypeOptions = [
+    { value: "Entire Room", label: "Entire Room", icon: "hotel" },
+    { value: "Room", label: "Room", icon: "meeting_room" },
+    { value: "AnyType", label: "AnyType", icon: "apartment" },
+  ];
+  const amenitiesOptions = [
+    { value: "Wifi", label: "Wifi", icon: "wifi" },
+    { value: "Kitchen", label: "Kitchen", icon: "kitchen" },
+    { value: "AC", label: "AC", icon: "ac_unit" },
+    {
+      value: "Washing Machine",
+      label: "Washing Machine",
+      icon: "local_laundry_service",
+    },
+    { value: "TV", label: "TV", icon: "tv" },
+    { value: "Pool", label: "Pool", icon: "pool" },
+    { value: "Free Parking", label: "Free Parking", icon: "local_parking" },
+  ];
+  const handleClearFilters = () => {
+    setPriceRange({ min: 600, max: 30000 });
+    setPropertyType("");
+    setRoomType("");
+    setAmenities([]);
+  };
+
+  const handleAmenitiesChange = (selectedAmenity) => {
+    setAmenities((PrevAmenities) =>
+      PrevAmenities.includes(selectedAmenity)
+        ? PrevAmenities.filter((item) => item !== selectedAmenity)
+        : [...PrevAmenities, selectedAmenity]
+    );
+  };
+  const handlePropertyTypeChange = (selectedType) => {
+    setPropertyType((PrevType) =>
+      PrevType === selectedType ? "" : selectedType
+    );
+  };
+
+  const handleRoomTypeChange = (selectedType) => {
+    setRoomType((PrevType) => (PrevType === selectedType ? "" : selectedType));
+  };
+
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        <h4>
+          Filters <hr />
+        </h4>
+        <button className="close-button" onClick={onClose}>
+          <span>&times;</span>
+        </button>
+        <div className="modal-filters-container">
+          <div className="filter-section"><label>Price range:</label></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FilterModal;
