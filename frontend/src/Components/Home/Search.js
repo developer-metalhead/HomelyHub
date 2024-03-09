f6ae008c7c7be7c0d0e89df41b5481404647fa98
developer-metalhead <satwikshah2000@gmail.com>
2024-03-09 15:40:22 +0530

    23 feb finish

--- frontend/src/Components/Home/Search.js
+++ frontend/src/Components/Home/Search.js
@@ -1,31 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { DatePicker, Space } from "antd";
+import moment from "moment";
 
 const Search = () => {
   const { RangePicker } = DatePicker;
+  const [keyword, setKeyword] = useState({});
+  const [value, setValue] = useState([null, null]); // Initialize value state with an array of null values
+
+  const returnDates = (date, dateString) => {
+    setValue(date);
+    updateKeyword("dateIn", dateString[0]);
+    updateKeyword("dateOut", dateString[1]);
+  };
+
+  const updateKeyword = (field, value) => {
+    setKeyword((prevKeyword) => ({ ...prevKeyword, [field]: value }));
+  };
+
   return (
-    <div className="searchbar">
-      <input
-        className="search"
-        id="search_destinaton"
-        placeholder="Search Destination"
-        type="text"
-      />
-      <Space direction="vertical" size={12} className="search">
-        <RangePicker
-          format="YYYY-MM-DD"
-          picker="date"
-          className="date_picker"
+    <>
+      <div className="searchbar">
+        <input
+          className="search"
+          id="search_destination"
+          placeholder="Search Destination"
+          type="text"
+          value={keyword.city || ""}
+          onChange={(e) => updateKeyword("city", e.target.value)}
         />
-      </Space>
-      <input
-        className="search"
-        id="addguest"
-        placeholder="Add Guest"
-        type="number"
-      />
-      <span class="material-symbols-outlined searchicon">search</span>
-    </div>
+        <Space direction="vertical" size={12} className="search">
+          <RangePicker
+            value={value}
+            format="YYYY-MM-DD"
+            picker="date"
+            className="date_picker"
+            disabledDate={(current) => {
+              return current && current.isBefore(moment(), "day");
+            }}
+            onChange={returnDates}
+          />
+        </Space>
+        <input
+          className="search"
+          id="addguest"
+          placeholder="Add Guests"
+          type="number"
+          onChange={(e) => updateKeyword("guests", e.target.value)}
+        />
+        <span className="material-symbols-outlined searchicon">search</span>
+      </div>
+    </>
   );
 };
 
